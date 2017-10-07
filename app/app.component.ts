de9c985bc416bdb1fe095d78b46bb5fe71468fb5
Polina Rylova <mail@polinarya.ru>
2017-10-07 20:00:23 +0500

    Реализовала добавление новой задачи

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 
-// интерфейс - контракт/договор, описывающий, какие св-ва и методы должен иметь класс
-// перед названием интерфейса принято указывать букву I, чтобы явно указать, что это интерфейс, а не класс, например
+/* интерфейс - контракт/договор, описывающий, какие св-ва и методы должен иметь класс
+перед названием интерфейса принято указывать букву I, чтобы явно указать, что это интерфейс, а не класс, например
 interface ITodo {
     title: string;
     completed: boolean;
-}
+}*/
 // Ещё одним способом определения типа объекта является Класс. 
 // На основе Класса, в отличие от Интерфейса, мы можем создать объект (интерфейс просто описывает)
 
-const todos: ITodo[] = [
+class Todo {
+    constructor(public title: string, 
+                public completed: boolean = false){}
+}
+
+const todos: Todo[] = [
     {
         title: 'Изучить Angular 2 и CLI',
         completed: true
@@ -32,13 +37,20 @@ const todos: ITodo[] = [
 })
 export class AppComponent {
     title: string = 'Angular 2 ToDo';
-    todos: ITodo[] = todos;
+    todos: Todo[] = todos;
 
-    completeToggle(todo: ITodo) {
+    create(event: Event, title: string) {
+        event.preventDefault();
+        
+        let todo: Todo = new Todo(title);
+        this.todos.push(todo);
+    };
+
+    completeToggle(todo: Todo) {
        todo.completed = !todo.completed; 
     }
 
-    deleteTodo(todo: ITodo) {
+    deleteTodo(todo: Todo) {
         let index = this.todos.indexOf(todo);
 
         if(index > -1) {
